7f25d25b7ee028c4db33c5e9fd51d18f67ba8cff
009Schmitt <009Schmitt@gmail.com>
2021-02-10 17:20:21 -0300

    Projeto Exemplo

---
+++ MVCPresentationLayer/Controllers/AcompanhanteController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using BusinessLogicalLayer;
+using Common;
+using Metadata;
+using Microsoft.AspNetCore.Mvc;
+using MVCPresentationLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPresentationLayer.Controllers
+{
+    public class AcompanhanteController : Controller
+    {
+        IMapper _mapper;
+        IAcompanhanteService acompanhanteService;
+
+        public AcompanhanteController(IMapper mapper, IAcompanhanteService srv)
+        {
+            this.acompanhanteService = srv;
+            _mapper = mapper;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(AcompanhanteViewModel viewModel)
+        {
+            Acompanhante acompanhante = _mapper.Map<Acompanhante>(viewModel);
+
+            acompanhanteService.Insert(acompanhante);
+
+            return View();
+        }
+
+    }
+}
---
+++ MVCPresentationLayer/Models/AcompanhanteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPresentationLayer.Models
+{
+    public class AcompanhanteViewModel
+    {
+        public string NomeGuerra { get; set; }
+        public bool TemSilicone{ get; set; }
+    }
+}
